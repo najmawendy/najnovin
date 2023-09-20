954690d4e7d1f0023f490f9d24ee1ac09e6d854c
najmawendy <113319279+najmawendy@users.noreply.github.com>
2023-09-20 12:32:47 -0300

    Add files via upload

--- media.js
+++ media.js
@@ -1,3 +1,16 @@
 const form = document.querySelector("form")
 const resp1 = document.querySelector("h3")
 const resp2 = document.querySelector("h4")
+
+form.addEventListener("submit"),(e) =>{
+    e.preventDefaunt()
+
+    const nome = form.inNome.value
+    const nota1 = Number(form.inNota1.value)
+    const Nota2 = Number (form.inNota2.value)
+    const media = (nota1 + nota2)/2
+    resp1.innerText = 'Média das Notas ${media.tofixed(2)}'
+
+
+    
+}
